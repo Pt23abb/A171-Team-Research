d1dd36903226a23db2735a8131f1c82ca0870284
Manasa12222 <mm24ahw@herts.ac.uk>
2024-11-26 18:08:28 +0000

    Updated analysis.R file without errors

--- analysis.R
+++ analysis.R
@@ -1,11 +1,15 @@
+#tidyverse library
 library(tidyverse)
+
 #Dataset
 df <- read.csv("vw.csv")
+
 #Dependent variable
 x <- df$price
+
 #Independent variable
 y<- df$transmission
-#Dependent variable has 3 categories
 
-irwise.wilcox.test(y,x)
+#Dependent variable has 3 categories
+pairwise.wilcox.test(x,y)
 
