066f8ccf090974eb790da2a790a381a177217102
Akash-2824 <an24acg@herts.ac.uk>
2024-11-26 17:52:33 +0000

    modifed visualization.R and updated histogram code

--- visualization.R
+++ visualization.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)  #Initializing the tidyverse library
 
 df <- read.csv("vw.csv")  #Reading the csv file and storing the data set in df variable
 
@@ -23,4 +23,24 @@ legend("topleft", inset = 0.02,
        col = c("brown1", "grey", "blue"),
        title = "Transmission type", lty = 1:1, cex = 0.8)
 
+#Histogram
+h <-hist(df$price, breaks = 50,
+         main = "Histogram showing the used volkswagen car prices among
+                 Automatic, Manual and Semi-Auto transmission type.",
+         xlab = "Price in pounds",
+         ylab = "Frequency",
+         col = c("bisque"),
+         xlim = c(0,70000),
+         ylim = c(0,1200))
 
+y <-df$price #Storing dependent variable
+dtMin=min(y)   #Minimum value present in the data set
+dtMax=max(y)   #Maximum value present in the data set
+dtMean=mean(y) #Calculating the mean of the dependent variable
+dtSd=sd(y)     #calculating the standard deviation
+
+x <-seq(dtMin, dtMax, 1)
+yn <-dnorm(x, mean=dtMean, sd=dtSd) 
+box.size <- diff(h$mids[1:2]) *length(y)
+yn <-yn*box.size
+lines(x, yn, col="blue")
